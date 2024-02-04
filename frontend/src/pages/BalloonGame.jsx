60ebe11b117e2a0c9d42fe4ef3da20bb29aa7d9c
Knthbtsta <hffdfg12@gmail.com>
2024-02-04 23:22:52 +0800

    wdaw

--- frontend/src/pages/BalloonGame.jsx
+++ frontend/src/pages/BalloonGame.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { useSearchParams } from "react-router-dom";
-import { BsBalloonHeartFill } from "react-icons/bs";
 import "../balloon.css"; // Import CSS file for styles
+import axios from "axios"; // Import Axios for HTTP requests
 import { useSearchParams } from "react-router-dom";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const BalloonGame = () => {
-  // Hooks
   const [searchParams, setSearchParams] = useSearchParams();
   const id = searchParams.get("id");
   const dungeonName = searchParams.get("dungeonName");
@@ -16,6 +16,8 @@ const BalloonGame = () => {
   const [stars, setStars] = useState(0); // Initialize stars state
   const location = useLocation();
   const { item } = location.state;
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch initial stars count from the database
@@ -62,13 +64,6 @@ const BalloonGame = () => {
     }
   };
 
-  useEffect(() => {
-    if (typedWord && words[0]) {
-      setShowModal(typedWord.toLowerCase() === words[0].toLowerCase());
-    }
-  }, [typedWord, words[0]]);
-
-  // Helper functions
   const generateLettersArray = () => {
     const letters = [];
     for (let i = 65; i <= 90; i++) {
@@ -77,43 +72,17 @@ const BalloonGame = () => {
     return letters;
   };
 
-  useEffect(() => {
-    // Use a setTimeout to change the image every 2 seconds (adjust as needed)
-    const timeoutId = setTimeout(() => {
-      const nextImageIndex = (currentImageIndex + 1) % item.image.length;
-      setCurrentImageIndex(nextImageIndex);
-    }, 2000); // Change image every 2 seconds (adjust as needed)
-
-    // Cleanup the timeout when component unmounts or when the word changes
-    return () => clearTimeout(timeoutId);
-  }, [currentImageIndex, item.image]);
-
-  const handleHeartClick = (letter) => {
-    setTypedWord((prevTypedWord) => prevTypedWord + letter);
-  };
-
-  const handlePlayTextToSpeech = () => {
-    const utterance = new SpeechSynthesisUtterance(words);
-    window.speechSynthesis.speak(utterance);
-  };
-
-  const handleCancel = () => {
-    setTypedWord("");
-    setShowModal(false);
-  };
-
-  // Rendering
   const letters = generateLettersArray();
   const shuffledLetters = letters.sort(() => Math.random() - 0.5);
   const gridSize = 6;
   const numRows = Math.ceil(letters.length / gridSize);
   const numCols = gridSize;
   const grid = [];
-
+  let index = 0;
   for (let i = 0; i < numRows; i++) {
     const row = [];
     for (let j = 0; j < numCols; j++) {
-      row.push(shuffledLetters[i * numCols + j]);
+      row.push(shuffledLetters[index++]);
     }
     grid.push(row);
   }
@@ -130,11 +99,7 @@ const BalloonGame = () => {
   };
 
   const heartIcons = letters.map((letter, idx) => (
-    <button
-      key={idx}
-      className="heart-btn"
-      onClick={() => handleHeartClick(letter)}
-    >
+    <button key={idx} className="heart-btn" onClick={handleHeartClick}>
       <FontAwesomeIcon
         icon={faHeart}
         className="heart-icon heart-icon-border"
@@ -142,12 +107,6 @@ const BalloonGame = () => {
       />
     </button>
   ));
-
-  const [typedWord, setTypedWord] = useState("");
-
-  const handleHeartClick = (letter) => {
-    setTypedWord((prevTypedWord) => prevTypedWord + letter);
-  };
 
   const handlePlayTextToSpeech = () => {
     const utterance = new SpeechSynthesisUtterance(words);
@@ -158,90 +117,115 @@ const BalloonGame = () => {
 
   useEffect(() => {
     if (typedWord && words[0]) {
-      if (typedWord.toLowerCase() === words[0].toLowerCase()) {
+      const isMatch = typedWord.toLowerCase() === words[0].toLowerCase();
+      if (isMatch && !showModal) {
         setShowModal(true);
-      } else {
+        // Increment stars when word matches
+        const newStars = stars + 1;
+        setStars(newStars);
+        // Update stars count in the database
+        updateStarsCount(newStars);
+      } else if (!isMatch && showModal) {
         setShowModal(false);
       }
     }
-  }, [typedWord, words[0]]);
+  }, [typedWord, words, showModal, stars]);
 
   const handleCancel = () => {
     setTypedWord("");
     setShowModal(false);
+    navigate(`/speech?id=${id}&dungeonName=${dungeonName}`); // Navigate to the other page with URL parameters
   };
 
   return (
-    <div className="flex justify-center items-center h-full w-full bg-[url('/minigamebg.png')] bg-no-repeat bg-cover">
-      <div className="w-[45%] h-screen flex justify-center items-center">
-        <div className="p-5 bg-white rounded-[100px] border-[10px] border-black">
-          {grid.map((row, rowIndex) => (
-            <div
-              className="text-red-700 text-[40px]"
-              key={rowIndex}
-              style={{ display: "flex" }}
-            >
-              {row.map((letter, colIndex) => (
-                <div
-                  key={`${rowIndex}-${colIndex}`}
-                  style={{ padding: "5px", position: "relative" }}
-                  className={`animated-item ${
-                    showModal ? "pointer-events-none" : ""
-                  }`}
-                >
-                  <div className="letter">{letter}</div>
-                  {heartIcons[rowIndex * numCols + colIndex]}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+    <div className="h-screen w-full bg-[url('/minigamebg.png')] bg-no-repeat bg-cover">
+      <div className="text-[100px] text-black pl-10">
+        {" "}
+        <FontAwesomeIcon
+          icon={faStar}
+          style={{
+            color: "#FFD43B",
+            fontSize: "6rem",
+            paddingTop: "10px",
+          }} // Adjust the fontSize as needed
+          bounce
+        />
+        {user.stars}
       </div>
-      <div className="h-screen w-[55%] flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-          <div className="text-[300px] text-black">{dungeonName}</div>
-          <div className="text-[100px] gap-5">
-            <button
-              onClick={handlePlayTextToSpeech}
-              className="bg-green-600 text-white py-2 px-4 rounded-[100px]"
-            >
-              PLAY
-            </button>
-            <span className="text-[150px] text-black">{words[0]}</span>
-          </div>
-          <div className="pt-10">
-            <input
-              type="text"
-              value={typedWord}
-              readOnly
-              className="bg-white text-black text-[50px] rounded-[50px]"
-            />
-          </div>
-        </div>
-      </div>
-      {showModal && (
-        <div
-          id="modal"
-          className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50"
-        >
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <div>
-              <h2 className="text-2xl font-bold text-gray-800">
-                WELL DONE!!!!!
-              </h2>
-            </div>
-            <div className="flex flex-col justify-center items-center pt-10">
-              <button
-                type="button"
-                className="py-2.5 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                onClick={handleCancel}
+      <div className="flex justify-center items-center">
+        <div className="w-[45%] flex justify-center items-center">
+          <div className="p-5 bg-white rounded-[100px] border-[10px] border-black">
+            {grid.map((row, rowIndex) => (
+              <div
+                className="text-red-700 text-[40px]"
+                key={rowIndex}
+                style={{ display: "flex" }}
               >
-                Cancel
+                {row.map((letter, colIndex) => (
+                  <div
+                    key={`${rowIndex}-${colIndex}`}
+                    style={{ padding: "5px", position: "relative" }}
+                    onClick={() => handleLetterClick(letter)}
+                    className={`animated-item`}
+                  >
+                    <div className="letter">{letter}</div>
+                    {heartIcons[rowIndex * numCols + colIndex]}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-[55%] flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center">
+            <div className="text-[300px] text-black bounce-in">
+              {dungeonName}
+            </div>
+            <div className="text-[100px]">
+              <button
+                onClick={handlePlayTextToSpeech}
+                className="bg-green-600 text-white px-4 rounded-[50px]"
+              >
+                PLAY
               </button>
+              <span className="text-[150px] text-black px-10 bounce-in">
+                {words[0]}
+              </span>
+            </div>
+            <div className="">
+              <input
+                type="text"
+                value={typedWord}
+                readOnly
+                className="bg-white text-black text-center text-[50px] rounded-[50px] w-[400px] flex items-center justify-center" // Adjust width as needed
+              />
             </div>
           </div>
         </div>
-      )}
+        {showModal && (
+          <div
+            id="modal"
+            className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50"
+          >
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+              <div>
+                <h2 className="text-2xl font-bold text-gray-800">
+                  WELL DONE!!!!!
+                </h2>
+              </div>
+              <div className="flex flex-col justify-center items-center pt-10">
+                <button
+                  type="button"
+                  className="py-2.5 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
