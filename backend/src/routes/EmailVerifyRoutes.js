4a6a9590ea3ce12d610474e22b63bba428a3d148
Knthbtsta <hffdfg12@gmail.com>
2024-05-12 18:15:46 +0800

    UPDATED BACKEND

--- backend/src/routes/EmailVerifyRoutes.js
+++ backend/src/routes/EmailVerifyRoutes.js
@@ -1,23 +1,30 @@
 // routes/verification.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verificationController = require('../controllers/EmailVerifyController');
-const user = require('../models/userModel');
+const verificationController = require("../controllers/EmailVerifyController");
+const user = require("../models/UserModel");
 
-router.post('/send-verification-email', verificationController.sendVerificationEmail);
-router.post('/verify-email', async (req, res) => {
-    const { token } = req.query;
-console.log(token);
-    // Implement email verification logic here
-    try {
-        const result = await user.findByIdAndUpdate({_id:token}, {verified:true}, {new:true});
+router.post(
+  "/send-verification-email",
+  verificationController.sendVerificationEmail
+);
+router.post("/verify-email", async (req, res) => {
+  const { token } = req.query;
+  console.log(token);
+  // Implement email verification logic here
+  try {
+    const result = await user.findByIdAndUpdate(
+      { _id: token },
+      { verified: true },
+      { new: true }
+    );
 
-        // Example: Send a success response if email is verified
-        res.status(200).json(result);
-    } catch (error) {
-        console.error('Failed to verify email:', error);
-        // Example: Send an error response if verification fails
-        res.status(500).json({ error: 'Failed to verify email' });
-    }
+    // Example: Send a success response if email is verified
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Failed to verify email:", error);
+    // Example: Send an error response if verification fails
+    res.status(500).json({ error: "Failed to verify email" });
+  }
 });
 module.exports = router;
