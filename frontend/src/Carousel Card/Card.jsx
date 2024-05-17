5202d404cd8f63a62390c57415623f2fd819520a
Denmark B. Dorado <Denmark-D@PHILORE.COM>
2024-05-17 13:18:29 +0800

    updated

--- frontend/src/Carousel Card/Card.jsx
+++ frontend/src/Carousel Card/Card.jsx
@@ -17,12 +17,18 @@ function Card({
   onNextClick,
 }) {
   const [show, setShown] = useState(true);
-  const [buttonColors, setButtonColors] = useState(items.map(() => generateRandomColor()));
+  const [buttonColors, setButtonColors] = useState(
+    items.map(() => generateRandomColor())
+  );
 
   const props3 = useSpring({
     opacity: 1,
-    transform: show ? "scale(1.03) translateY(-10px)" : "scale(1) translateY(0)",
-    boxShadow: show ? "0 20px 25px rgb(0 0 0 / 25%)" : "0 2px 10px rgb(0 0 0 / 8%)",
+    transform: show
+      ? "scale(1.03) translateY(-10px)"
+      : "scale(1) translateY(0)",
+    boxShadow: show
+      ? "0 20px 25px rgb(0 0 0 / 25%)"
+      : "0 2px 10px rgb(0 0 0 / 8%)",
   });
 
   const props4 = useSpring({
@@ -45,7 +51,11 @@ function Card({
   return (
     <div
       className="relative bg-cover bg-center"
-      style={{ backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundPosition: "center" }}
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
       onClick={onCardClick}
     >
       {showNavigation && (
@@ -64,22 +74,30 @@ function Card({
       )}
       <div className="">
         <animated.div
-          className="bg-cover bg-center shadow-lg rounded-2xl border-2 border-[#e9dfb2] w-[200px]  sm:w-[20rem] md:w-[25rem] p-4 sm:p-8 md:p-12"
+          className="bg-cover bg-centershadow-lg rounded-4xl border-2 border-[#e9dfb2] w-[15rem]  sm:w-[20rem] md:w-[30rem] "
           style={{ backgroundImage: `url(${img})`, ...props3 }}
         >
-          <div className={`flex flex-col justify-center text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl pt-4 sm:pt-8 ${color}`}></div>
-          <animated.img src={imagen} alt="Your Image" style={show ? props4 : null} />
-          <div className="flex flex-col justify-center items-center mt-4 pt-6 pb-4">
+          <animated.img
+            src={imagen}
+            alt="Your Image"
+            style={show ? props4 : null}
+          />
+          <div className="flex flex-col justify-center rounded-xl items-center  mt-4 pt-6 pb-4">
             {items.map((item, index) => (
               <Link
                 to={link}
                 state={{ item }}
                 key={index}
                 className={`py-2 px-4 sm:px-6 md:px-8 rounded-xl mt-4 sm:mt-8 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl tracking-wide transition transform hover:scale-105 duration-300 ${
-                  dungeonName === "Aa" ? "bg-yellow-300 text-white" :
-                  dungeonName === "Bb" ? "bg-cyan-500 text-white" :
-                  dungeonName === "Cc" ? "bg-red-700 text-white" :
-                  dungeonName === "Dd" ? "bg-violet-600 text-white" : ""
+                  dungeonName === "Aa"
+                    ? "bg-yellow-300 text-white"
+                    : dungeonName === "Bb"
+                    ? "bg-cyan-500 text-white"
+                    : dungeonName === "Cc"
+                    ? "bg-red-700 text-white"
+                    : dungeonName === "Dd"
+                    ? "bg-violet-600 text-white"
+                    : ""
                 }`}
                 style={{ backgroundColor: buttonColors[index] }}
                 onClick={() => handlePlayButtonClick(index)}
